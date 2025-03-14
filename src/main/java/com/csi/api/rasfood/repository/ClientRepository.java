12016021c40a4424cf99c9847594b79bd262903e
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-14 16:30:30 -0300

    feat: Adicionado interface ClientRepository
    Criando interface ClientRepository para manipulação de dados.

--- src/main/java/com/csi/api/rasfood/repository/ClientRepository.java
+++ src/main/java/com/csi/api/rasfood/repository/ClientRepository.java
@@ -1,4 +1,9 @@
 package com.csi.api.rasfood.repository;
 
-public interface ClientRepository {
-}
+import com.csi.api.rasfood.entity.Client;
+import com.csi.api.rasfood.entity.ClientId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClientRepository extends JpaRepository<Client, ClientId> {}
