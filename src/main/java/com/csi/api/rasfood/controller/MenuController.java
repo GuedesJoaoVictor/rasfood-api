57e0e9cfac33d4cd145d02f106fee18e8047de0f
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-14 16:29:00 -0300

    feat: Classe teste para o Menu
    Criando classe para testar o funcionamento do spring, usando a classe de
    MenuController

--- src/main/java/com/csi/api/rasfood/controller/MenuController.java
+++ src/main/java/com/csi/api/rasfood/controller/MenuController.java
@@ -1,4 +1,15 @@
 package com.csi.api.rasfood.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping(value = "/menu")
+@RestController
 public class MenuController {
+
+    @GetMapping(value = "/test")
+    private String test() {
+        return "I'm Alive!";
+    }
 }
