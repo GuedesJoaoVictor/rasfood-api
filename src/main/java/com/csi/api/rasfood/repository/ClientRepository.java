aa0d499b1d3e996e8aa62db99104b9458aca1d6a
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-15 12:44:23 -0300

    feat: Criando query de findByEmailOrCpf
    Criando query para achar um cliente independente se nos for provido o
    email ou cpf

--- src/main/java/com/csi/api/rasfood/repository/ClientRepository.java
+++ src/main/java/com/csi/api/rasfood/repository/ClientRepository.java
@@ -3,7 +3,16 @@ package com.csi.api.rasfood.repository;
 import com.csi.api.rasfood.entity.Client;
 import com.csi.api.rasfood.entity.ClientId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface ClientRepository extends JpaRepository<Client, ClientId> {}
+public interface ClientRepository extends JpaRepository<Client, ClientId> {
+
+    @Query("select c from Client c where c.clientId.email = :id or c.clientId.cpf = :id")
+    Optional<Client> findByEmailOrCpf(@Param("id") String id);
+
+}
