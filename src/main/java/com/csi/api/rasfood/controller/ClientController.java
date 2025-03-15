ae4b65816c599ad7658607e7319c445b244b15ca
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-15 12:45:59 -0300

    feat: Criando rota de atualizar cliente
    Criando rota patch para atualizar o cliente, podendo alterar apenas uma
    linha por vez. Devendo ser provido o email ou cpf do cliente na url

--- src/main/java/com/csi/api/rasfood/controller/ClientController.java
+++ src/main/java/com/csi/api/rasfood/controller/ClientController.java
@@ -3,13 +3,12 @@ package com.csi.api.rasfood.controller;
 import com.csi.api.rasfood.entity.Client;
 import com.csi.api.rasfood.entity.ClientId;
 import com.csi.api.rasfood.repository.ClientRepository;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,18 +20,31 @@ public class ClientController {
     @Autowired
     private ClientRepository clientRepository;
 
+    private ObjectMapper mapper = new ObjectMapper();
+
     @GetMapping
-    private ResponseEntity<List<Client>> findAll() {
+    public ResponseEntity<List<Client>> findAll() {
         return ResponseEntity.status(HttpStatus.OK).body(clientRepository.findAll());
     }
 
     @GetMapping("/{cpf}/{email}")
-    private ResponseEntity<Optional<Client>> findById(@PathVariable("cpf") String cpf, @PathVariable("email") String email) {
+    public ResponseEntity<Optional<Client>> findById(@PathVariable("cpf") String cpf, @PathVariable("email") String email) {
         ClientId id = new ClientId(cpf, email);
         Optional<Client> client = clientRepository.findById(id);
-        if(!client.isPresent()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Optional.empty());
+        return client.map(value -> ResponseEntity.status(HttpStatus.OK).body(clientRepository.findById(value.getClientId())))
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).body(Optional.empty()));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Client> update(@PathVariable("id") String id, @RequestBody Client client) throws JsonMappingException {
+        Optional<Client> currentClient = clientRepository.findByEmailOrCpf(id);
+
+        if(currentClient.isPresent()) {
+            Client existingClient = currentClient.get();
+            client.setClientId(existingClient.getClientId());
+            mapper.updateValue(existingClient, client);
+            return ResponseEntity.status(HttpStatus.OK).body(clientRepository.save(currentClient.get()));
         }
-        return ResponseEntity.status(HttpStatus.OK).body(clientRepository.findById(client.get().getClientId()));
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
     }
 }
