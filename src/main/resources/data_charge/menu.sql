603c0fd9ec87d48b65f6d160862a848fe194d4c9
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-18 11:16:19 -0300

    feat: Adicionando dados ao banco
    Adicionando dados de cardapio e de categoria ao banco de dados.

---
+++ src/main/resources/data_charge/menu.sql
@@ -0,0 +1,35 @@
+INSERT INTO menu (name, description, status, price, menu_category_id, register_date)
+VALUES ('Bolinhos de Bacalhau', 'Bolinhos crocantes de bacalhau com molho tártaro', true, '25.00', 1, NOW());
+
+INSERT INTO menu (name, description, status, price, menu_category_id, register_date)
+VALUES ('Carpaccio de Filé Mignon', 'Finas fatias de filé mignon com rúcula e parmesão', true, '35.00', 1, NOW());
+
+INSERT INTO menu (name, description, status, price, menu_category_id, register_date)
+VALUES ('Bruschetta de Tomate e Manjericão', 'Pão italiano com tomate fresco, manjericão e azeite', true, '18.00', 1, NOW());
+
+INSERT INTO menu (name, description, status, price, menu_category_id, register_date)
+VALUES ('Salada Caesar', 'Alface romana, croutons, parmesão e molho Caesar', true, '28.00', 2, NOW());
+
+INSERT INTO menu (name, description, status, price, menu_category_id, register_date)
+VALUES ('Salada de Quinoa', 'Quinoa, grão-de-bico, tomate cereja e rúcula', true, '32.00', 2, NOW());
+
+INSERT INTO menu (name, description, status, price, menu_category_id, register_date)
+VALUES ('Salada Tropical', 'Manga, abacaxi, rúcula e molho de laranja', true, '30.00', 2, NOW());
+
+INSERT INTO menu (name, description, status, price, menu_category_id, register_date)
+VALUES ('Filé Mignon ao Molho Madeira', 'Filé mignon grelhado com molho madeira e purê de batatas', true, '120.00', 3, NOW());
+
+INSERT INTO menu (name, description, status, price, menu_category_id, register_date)
+VALUES ('Risoto de Camarão', 'Risoto cremoso com camarões frescos e ervas finas', true, '85.00', 3, NOW());
+
+INSERT INTO menu (name, description, status, price, menu_category_id, register_date)
+VALUES ('Frango à Parmegiana', 'Filé de frango empanado com molho de tomate e queijo derretido', true, '55.00', 3, NOW());
+
+INSERT INTO menu (name, description, status, price, menu_category_id, register_date)
+VALUES ('Cheesecake de Frutas Vermelhas', 'Cheesecake com calda de frutas vermelhas', true, '22.00', 4, NOW());
+
+INSERT INTO menu (name, description, status, price, menu_category_id, register_date)
+VALUES ('Petit Gateau', 'Bolo de chocolate com recheio cremoso e sorvete de baunilha', true, '28.00', 4, NOW());
+
+INSERT INTO menu (name, description, status, price, menu_category_id, register_date)
+VALUES ('Pudim de Leite Condensado', 'Pudim tradicional com calda de caramelo', true, '15.00', 4, NOW());
---
+++ src/main/resources/data_charge/menu_category.sql
@@ -0,0 +1,4 @@
+insert into menu_category(name) values ('Entradas'),
+                                       ('Saladas'),
+                                       ('Pratos Principais'),
+                                       ('Sobremesas');
