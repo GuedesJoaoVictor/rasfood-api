9a26ed3bad877d46aaf1203abf3b6d488b421402
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-19 00:24:00 -0300

    feat: Adicionando classe MenuCategoryController
    Iniciando classe de MenuCaregoryController e adicionando endpoint de
    findAll

--- src/main/java/com/csi/api/rasfood/controller/MenuCategoryController.java
+++ src/main/java/com/csi/api/rasfood/controller/MenuCategoryController.java
@@ -1,4 +1,28 @@
 package com.csi.api.rasfood.controller;
 
+import com.csi.api.rasfood.entity.MenuCategory;
+import com.csi.api.rasfood.repository.MenuCategoryRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/menu-category")
 public class MenuCategoryController {
+    @Autowired
+    private MenuCategoryRepository menuCategoryRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @GetMapping
+    public ResponseEntity<List<MenuCategory>> findAll() {
+        return ResponseEntity.status(HttpStatus.OK).body(menuCategoryRepository.findAll());
+    }
 }
