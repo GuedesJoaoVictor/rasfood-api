54f778d9f0585fa4f0d657449f5ac7eaea4f70ae
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-19 10:14:26 -0300

    feat: Adicionando query ao MenuRepository
    Adicionando query de procurar por categoria no cardapio.

--- src/main/java/com/csi/api/rasfood/repository/MenuRepository.java
+++ src/main/java/com/csi/api/rasfood/repository/MenuRepository.java
@@ -2,7 +2,13 @@ package com.csi.api.rasfood.repository;
 
 import com.csi.api.rasfood.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface MenuRepository extends JpaRepository<Menu, Long> {}
+public interface MenuRepository extends JpaRepository<Menu, Long> {
+    @Query(value = "select * from menu where menu_category_id = ?1 and status = true", nativeQuery = true)
+    List<Menu> findAllByCategory(Long category);
+}
