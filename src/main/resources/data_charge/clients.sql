31801dc22af516b7b3f47e69b5ca7760076806a8
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-04-08 16:40:34 -0300

    fix: Ajustando sql de clientes

--- src/main/resources/data_charge/clients.sql
+++ src/main/resources/data_charge/clients.sql
@@ -1,29 +1,10 @@
-INSERT INTO addresses (cep, city, complement, state, street, client_cpf, client_email)
-VALUES ('01001000', 'São Paulo', 'Apto 101', 'SP', 'Avenida Paulista', '12345678901', 'maria.silva@email.com');
-
-INSERT INTO addresses (cep, city, complement, state, street, client_cpf, client_email)
-VALUES ('20040000', 'Rio de Janeiro', 'Sala 205', 'RJ', 'Rua Copacabana', '23456789012', 'joao.oliveira@email.com');
-
-INSERT INTO addresses (cep, city, complement, state, street, client_cpf, client_email)
-VALUES ('30130000', 'Belo Horizonte', 'Casa 3', 'MG', 'Rua das Palmeiras', '34567890123', 'ana.costa@email.com');
-
-INSERT INTO addresses (cep, city, complement, state, street, client_cpf, client_email)
-VALUES ('40010000', 'Salvador', 'Bloco B', 'BA', 'Avenida Oceânica', '45678901234', 'pedro.santos@email.com');
-
-INSERT INTO addresses (cep, city, complement, state, street, client_cpf, client_email)
-VALUES ('50010000', 'Recife', 'Apto 302', 'PE', 'Rua da Aurora', '56789012345', 'carla.lima@email.com');
-
-INSERT INTO addresses (cep, city, complement, state, street, client_cpf, client_email)
-VALUES ('60010000', 'Fortaleza', 'Cobertura', 'CE', 'Avenida Beira Mar', '67890123456', 'lucas.pereira@email.com');
-
-INSERT INTO addresses (cep, city, complement, state, street, client_cpf, client_email)
-VALUES ('70040000', 'Brasília', 'Sala 10', 'DF', 'Quadra 102', '78901234567', 'fernanda.rocha@email.com');
-
-INSERT INTO addresses (cep, city, complement, state, street, client_cpf, client_email)
-VALUES ('80010000', 'Curitiba', 'Apto 501', 'PR', 'Rua das Araucárias', '89012345678', 'rafael.almeida@email.com');
-
-INSERT INTO addresses (cep, city, complement, state, street, client_cpf, client_email)
-VALUES ('90010000', 'Porto Alegre', 'Casa 2', 'RS', 'Avenida Ipiranga', '90123456789', 'juliana.barbosa@email.com');
-
-INSERT INTO addresses (cep, city, complement, state, street, client_cpf, client_email)
-VALUES ('80050000', 'Florianópolis', 'Bloco C', 'SC', 'Rua das Gaivotas', '01234567890', 'marcos.souza@email.com');
+INSERT INTO clients VALUES ('12345678901', 'maria.silva@email.com', '11', '987654321');
+INSERT INTO clients VALUES ('23456789012', 'joao.oliveira@email.com', '21', '987654321');
+INSERT INTO clients VALUES ('34567890123', 'ana.costa@email.com', '31', '987654321');
+INSERT INTO clients VALUES ('45678901234', 'pedro.santos@email.com', '71', '987654321');
+INSERT INTO clients VALUES ('56789012345', 'carla.lima@email.com', '81', '987654321');
+INSERT INTO clients VALUES ('67890123456', 'lucas.pereira@email.com', '85', '987654321');
+INSERT INTO clients VALUES ('78901234567', 'fernanda.rocha@email.com', '61', '987654321');
+INSERT INTO clients VALUES ('89012345678', 'rafael.almeida@email.com', '41', '987654321');
+INSERT INTO clients VALUES ('90123456789', 'juliana.barbosa@email.com', '51', '987654321');
+INSERT INTO clients VALUES ('01234567890', 'marcos.souza@email.com', '48', '987654321');
